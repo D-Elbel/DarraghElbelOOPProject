daadb0fe08204c150c8b7b2c76b6bfd94b6db096
Darragh <elbeldarragh@gmail.com>
2021-12-03 11:49:02 +0000

    Final Commit Before Requirements File

---
+++ project/MotorcycleApp.java
@@ -0,0 +1,258 @@
+package project;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+public class MotorcycleApp extends JFrame implements ActionListener {
+
+    private static JFrame addMotorcycleFrame, addServiceFrame, addPartFrame, viewMotorcycles;
+    private JMenu addMotorcycle;
+    private JMenu viewMotorcycle;
+    private JMenu addService;
+    private JMenu addPart;
+
+    ArrayList<Motorcycle> motorcycles = new ArrayList<>();
+
+    public MotorcycleApp(){
+
+        createAddMenu();
+        createViewMenu();
+        createAddServiceMenu();
+        createAddPartMenu();
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        menuBar.setBackground(Color.lightGray);
+        menuBar.add(addMotorcycle);
+        menuBar.add(viewMotorcycle);
+        menuBar.add(addService);
+        menuBar.add(addPart);
+
+        setTitle("My Motorcycle Manager");
+        setSize(650, 400);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        /*******************************************************************
+         *Title: How to set Icon to JFrame
+                *Author: Airy
+                *Site owner/sponsor: stackoverflow.com
+                *Date: Oct 27 2013
+                *Code Version: Oct 27 2013
+                *Availability: https://stackoverflow.com/questions/15657569/how-to-set-icon-to-jframe(Accessed: 03/12/2021)
+         *Modified: Using my own image path
+         *******************************************************************/
+        JLabel imgLabel = new JLabel();
+        imgLabel.setIcon(new ImageIcon(getClass().getResource("javaprojectlogo.png")));
+
+        this.add(imgLabel);
+        setLayout(new FlowLayout());
+
+        open();
+    }
+
+    public void actionPerformed(ActionEvent event){
+        String menuName;
+        menuName = event.getActionCommand();
+
+        if(event.getSource().equals(addMotorcycleFrame)){
+            addMotorcycleFrame.dispose();
+        }
+
+        if(menuName.equals("Add Motorcycle")){
+            addMotorcycleFrame = new JFrame("Add Motorcycle");
+            AddWithoutVinForm gui = new AddWithoutVinForm();
+            addMotorcycleFrame.add(gui.getPanel1());
+            addMotorcycleFrame.setLocationRelativeTo(null);
+            addMotorcycleFrame.setSize(450, 500);
+            addMotorcycleFrame.setVisible(true);
+        }
+
+        if(menuName.equals("Add Service")){
+            addServiceFrame = new JFrame("Add Service");
+            AddServiceForm serviceGui = new AddServiceForm();
+            addServiceFrame.add(serviceGui.getPanel1());
+            addServiceFrame.setLocationRelativeTo(null);
+            addServiceFrame.setSize(450,500);
+            addServiceFrame.setVisible(true);
+        }
+
+        if(menuName.equals("Add Part")){
+            addPartFrame = new JFrame("Add Service");
+            AddPartForm partGui = new AddPartForm();
+            addPartFrame.add(partGui.getPanel1());
+            addPartFrame.setLocationRelativeTo(null);
+            addPartFrame.setSize(450,500);
+            addPartFrame.setVisible(true);
+        }
+
+        if(menuName.equals("Add Without VIN")){
+            addMotorcycleFrame.setVisible(true);
+            System.out.println("Test");
+        }
+
+        if(menuName.equals("View List")){
+
+            viewMotorcycles = new JFrame("View");
+            viewMotorcycles viewGui = new viewMotorcycles();
+            viewMotorcycles.add(viewGui.getPanel1());
+            viewMotorcycles.setLocationRelativeTo(null);
+            viewMotorcycles.setSize(650,400);
+            viewMotorcycles.setVisible(true);
+        }
+
+        if(menuName.equals("Quit")){
+            System.exit(0);
+        }
+
+    }
+
+    private void createAddMenu() {
+
+        JMenuItem item;
+
+        addMotorcycle = new JMenu("Add Motorcycles");
+
+        item = new JMenuItem("Add Motorcycle");
+        item.addActionListener(this);
+        addMotorcycle.add(item);
+    }
+
+    private void createViewMenu() {
+        JMenuItem item;
+
+        viewMotorcycle = new JMenu("View Motorcycles");
+        item = new JMenuItem("View List");
+        item.addActionListener(this);
+        viewMotorcycle.add(item);
+    }
+
+    private void createAddServiceMenu() {
+        JMenuItem item;
+
+        addService = new JMenu("Add Service");
+
+        item = new JMenuItem("Add Service");
+        item.addActionListener(this);
+        addService.add(item);
+
+    }
+
+    private void createAddPartMenu() {
+        JMenuItem item;
+
+        addPart = new JMenu("Add Part");
+
+        item = new JMenuItem("Add Part");
+        item.addActionListener(this);
+        addPart.add(item);
+    }
+
+
+    public static void main(String[] args) {
+        MotorcycleApp frame = new MotorcycleApp();
+        frame.setVisible(true);
+    }
+
+    public void save() throws IOException {
+        ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("motorcycles.dat"));
+        os.writeObject(motorcycles);
+        os.close();
+    }
+
+    /*This is where I found .dispose() https://stackoverflow.com/questions/13360430/jframe-dispose-vs-system-exit#:~:text=dispose()%3B%20causes%20the%20JFrame,effect%20rather%20than%20the%20norm.
+    * needed alternative to System.exit(0) as it closes all open JFrames and setVisible(false) wouldn't reset input fields*/
+    public void closeFrame() throws IOException {
+        addMotorcycleFrame.dispose();
+    }
+
+    public void closeServiceFrame() throws IOException {
+        addServiceFrame.dispose();
+    }
+
+    public void closePartFrame() throws IOException {
+        addPartFrame.dispose();
+    }
+
+    public void closeViewFrame() throws IOException {
+        viewMotorcycles.dispose();
+    }
+
+    public void open() {
+
+        try {
+            File file = new File("motorcycles.dat");
+
+            if(file.exists()) {
+
+                ObjectInputStream is = new ObjectInputStream(new FileInputStream(file));
+                motorcycles = (ArrayList<Motorcycle>) is.readObject();
+                is.close();
+
+                JOptionPane.showMessageDialog(null, file.getName() + " file loaded into the system", "Open", JOptionPane.INFORMATION_MESSAGE);
+            }
+            else{
+                file.createNewFile();
+                JOptionPane.showMessageDialog(null, "File just created!!", "Created " + file.getName() + " file", JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
+        catch(ClassNotFoundException cce) {
+            JOptionPane.showMessageDialog(null,"Class of object deserialized not a match for anything used in this application","Error",JOptionPane.ERROR_MESSAGE);
+            cce.printStackTrace();
+        }
+        catch (IOException ioe) {
+            JOptionPane.showMessageDialog(null,"Problem reading from the file","Error",JOptionPane.ERROR_MESSAGE);
+            ioe.printStackTrace();
+        }
+    }
+
+    public GregorianCalendar getDateString(String serviceDateAsString) {
+        int serviceYear = 2000, serviceMonth = 1, serviceDay = 1;
+
+        String yearSub, monthSub, daySub;
+        yearSub = serviceDateAsString.substring(0,4);
+        monthSub = serviceDateAsString.substring(5,7);
+        daySub = serviceDateAsString.substring(8,10);
+
+        serviceYear = Integer.parseInt(serviceDateAsString.substring(0,4));
+        serviceMonth = Integer.parseInt(serviceDateAsString.substring(5,7));
+        serviceDay = Integer.parseInt(serviceDateAsString.substring(8,10));
+
+        for(int i = 0; i < yearSub.length(); i++){
+            if(!Character.isDigit(yearSub.charAt(i))){
+                JOptionPane.showMessageDialog(null, "Year is not valid, setting to default of 2000");
+                serviceYear = 2000;
+                i = yearSub.length();
+            }
+        }
+
+        for(int i = 0; i < monthSub.length(); i++){
+            if(!Character.isDigit(yearSub.charAt(i))){
+                JOptionPane.showMessageDialog(null, "Month is not valid, setting to default of 1");
+                serviceMonth = 1;
+                i = monthSub.length();
+            }
+        }
+
+        for(int i = 0; i < daySub.length(); i++){
+            if(!Character.isDigit(daySub.charAt(i))){
+                JOptionPane.showMessageDialog(null, "Day is not valid, setting to default of 1");
+                serviceDay = 1;
+                i = daySub.length();
+            }
+        }
+
+        if(serviceDateAsString.length() != 10){
+            JOptionPane.showMessageDialog(null, "Invalid Date Length, setting to default", "Invalid Length", JOptionPane.WARNING_MESSAGE);
+        }
+
+        return new GregorianCalendar(serviceYear, serviceMonth, serviceDay);
+    }
+
+}
---
+++ project/viewMotorcycles.java
@@ -0,0 +1,205 @@
+package project;
+
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class viewMotorcycles extends MotorcycleApp {
+    private JComboBox motorcycleDropdown;
+    private JButton viewSubmitButton;
+    private JPanel panel1;
+    private JTextArea motorcycleTextArea;
+    private JTextArea partsTextArea;
+    private JTextArea serviceHistoryTextArea;
+    private JButton deleteMotorcycle;
+    private int selected;
+
+    public viewMotorcycles() {
+
+        $$$setupUI$$$();
+
+        motorcycleTextArea.setLineWrap(true);
+
+        motorcycleDropdown.addActionListener(this);
+
+        JTextArea output = new JTextArea();
+
+        output.setText("Motorcycle Details:\n\n");
+
+        if (viewMotorcycles.super.motorcycles.size() < 1) {
+            JOptionPane.showMessageDialog(null, "No motorcycles have been added to the system", "Warning", JOptionPane.WARNING_MESSAGE);
+        } else {
+
+            for (Motorcycle motorcycle : viewMotorcycles.super.motorcycles) {
+                motorcycleDropdown.addItem(motorcycle.getModelName() + "\n");
+            }
+
+            selected = motorcycleDropdown.getSelectedIndex();
+        }
+        viewSubmitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                motorcycleTextArea.setText("");
+                serviceHistoryTextArea.setText("");
+                partsTextArea.setText("");
+
+                selected = motorcycleDropdown.getSelectedIndex();
+
+                if (!viewMotorcycles.super.motorcycles.get(selected).getVin().equals("00000000000000000")) {
+                    motorcycleTextArea.append("VIN: " + viewMotorcycles.super.motorcycles.get(selected).getVin());
+                }
+                motorcycleTextArea.append("\n\nManufacturer: " + viewMotorcycles.super.motorcycles.get(selected).getManufacturer());
+                motorcycleTextArea.append("\n\nModel Name: " + viewMotorcycles.super.motorcycles.get(selected).getModelName());
+                motorcycleTextArea.append("\n\nYear: " + viewMotorcycles.super.motorcycles.get(selected).getYearCode());
+                motorcycleTextArea.append("\n\nCountry of Origin: " + viewMotorcycles.super.motorcycles.get(selected).getCountryOfOrigin());
+                motorcycleTextArea.append("\n\nEngine Size: " + viewMotorcycles.super.motorcycles.get(selected).getEngineDisplacement());
+                motorcycleTextArea.append("\n\nEngine Type: " + viewMotorcycles.super.motorcycles.get(selected).getEngineType());
+
+                if (viewMotorcycles.super.motorcycles.get(selected).getOdometer() != 0) {
+                    motorcycleTextArea.append("\n\nEngine Type: " + viewMotorcycles.super.motorcycles.get(selected).getOdometer());
+                }
+
+                if (!viewMotorcycles.super.motorcycles.get(selected).getVis().equals("null")) {
+                    motorcycleTextArea.append("\n\nVIS: " + viewMotorcycles.super.motorcycles.get(selected).getVis());
+                }
+
+
+                String serviceOutput = "", partsOutput = "";
+
+                for (int i = 0; i < viewMotorcycles.super.motorcycles.get(selected).getServiceHistory().length; i++) {
+                    if (viewMotorcycles.super.motorcycles.get(selected).getServiceHistory()[i] != null) {
+                        serviceOutput += viewMotorcycles.super.motorcycles.get(selected).getServiceHistory()[i];
+                    } else {
+                        i = viewMotorcycles.super.motorcycles.get(selected).getServiceHistory().length;
+                    }
+                }
+
+
+                for (int i = 0; i < viewMotorcycles.super.motorcycles.get(selected).getPartList().length; i++) {
+                    if (viewMotorcycles.super.motorcycles.get(selected).getPartList()[i] != null) {
+                        partsOutput += viewMotorcycles.super.motorcycles.get(selected).getPartList()[i];
+                    } else {
+                        i = viewMotorcycles.super.motorcycles.get(selected).getPartList().length;
+                    }
+                }
+
+                serviceHistoryTextArea.append(serviceOutput);
+
+                partsTextArea.append(partsOutput);
+            }
+        });
+        deleteMotorcycle.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                selected = motorcycleDropdown.getSelectedIndex();
+
+                System.out.print(selected);
+
+                project.viewMotorcycles.super.motorcycles.remove(selected);
+
+                JOptionPane.showMessageDialog(null, "Motorcycle Deleted");
+
+                try {
+                    viewMotorcycles.super.save();
+                    viewMotorcycles.super.closeViewFrame();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public JPanel getPanel1() {
+        return panel1;
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        panel1 = new JPanel();
+        panel1.setLayout(new BorderLayout(0, 0));
+        panel1.setMinimumSize(new Dimension(650, 350));
+        panel1.setPreferredSize(new Dimension(650, 350));
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 1));
+        panel2.setMinimumSize(new Dimension(650, 32));
+        panel2.setPreferredSize(new Dimension(316, 50));
+        panel2.putClientProperty("html.disable", Boolean.FALSE);
+        panel1.add(panel2, BorderLayout.NORTH);
+        motorcycleDropdown = new JComboBox();
+        panel2.add(motorcycleDropdown);
+        viewSubmitButton = new JButton();
+        viewSubmitButton.setText("Submit");
+        panel2.add(viewSubmitButton);
+        deleteMotorcycle = new JButton();
+        deleteMotorcycle.setText("Delete Selected");
+        panel2.add(deleteMotorcycle);
+        final JPanel panel3 = new JPanel();
+        panel3.setLayout(new GridBagLayout());
+        panel3.setMinimumSize(new Dimension(650, 350));
+        panel3.setPreferredSize(new Dimension(500, 300));
+        panel1.add(panel3, BorderLayout.CENTER);
+        final JScrollPane scrollPane1 = new JScrollPane();
+        scrollPane1.setMinimumSize(new Dimension(200, 300));
+        scrollPane1.setPreferredSize(new Dimension(200, 300));
+        GridBagConstraints gbc;
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.BOTH;
+        panel3.add(scrollPane1, gbc);
+        scrollPane1.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), null, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
+        serviceHistoryTextArea = new JTextArea();
+        serviceHistoryTextArea.setLineWrap(true);
+        serviceHistoryTextArea.setMinimumSize(new Dimension(100, 300));
+        serviceHistoryTextArea.setPreferredSize(new Dimension(150, 600));
+        scrollPane1.setViewportView(serviceHistoryTextArea);
+        final JScrollPane scrollPane2 = new JScrollPane();
+        scrollPane2.setMinimumSize(new Dimension(200, 300));
+        scrollPane2.setPreferredSize(new Dimension(200, 300));
+        gbc = new GridBagConstraints();
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.BOTH;
+        panel3.add(scrollPane2, gbc);
+        scrollPane2.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), null, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
+        partsTextArea = new JTextArea();
+        partsTextArea.setLineWrap(true);
+        partsTextArea.setMinimumSize(new Dimension(100, 300));
+        partsTextArea.setPreferredSize(new Dimension(150, 600));
+        scrollPane2.setViewportView(partsTextArea);
+        final JScrollPane scrollPane3 = new JScrollPane();
+        scrollPane3.setMinimumSize(new Dimension(200, 350));
+        scrollPane3.setPreferredSize(new Dimension(200, 300));
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.BOTH;
+        panel3.add(scrollPane3, gbc);
+        scrollPane3.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), null, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
+        motorcycleTextArea = new JTextArea();
+        motorcycleTextArea.setEditable(false);
+        motorcycleTextArea.setLineWrap(true);
+        motorcycleTextArea.setMinimumSize(new Dimension(100, 350));
+        motorcycleTextArea.setPreferredSize(new Dimension(150, 600));
+        scrollPane3.setViewportView(motorcycleTextArea);
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return panel1;
+    }
+
+}
